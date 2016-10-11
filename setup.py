0bd02429d241a4523730be20b4fb15894e2c76e5
Ansley Peduru <ansleypeduru96@gmail.com>
2016-10-11 14:01:03 -0400

    More descriptive setup

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-import distutils
+#!/usr/bin/python3
 
-distutils.core.setup(
+from distutils.core import setup
+
+setup(
     name='sdfi',
     version='0.1',
-    scripts=['sdfi']
+    author='Ansley Peduru',
+    author_email='ansleypeduru96@gmail.com',
+    url='https://github.com/apeduru/sdfi',
+    scripts=['script/sdfi']
 )
