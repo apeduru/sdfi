cf24d8868d933459a0a23822f26a3474c1e4f829
Ansley Peduru <ansleypeduru96@gmail.com>
2016-10-14 18:17:43 -0400

    Fix unit tests

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,17 +1,16 @@
 import unittest
 from sdfi import scheduler, tokenizer
-import re
 from collections import Counter
 
 
 class TestScheduler(unittest.TestCase):
     def setUp(self):
         self.test1 = 'texts/test1.txt'
-        self.test1_result = [('Ansley', 1), ('Peduru', 1)]
+        self.test1_result = [('ansley', 5), ('peduru', 5)]
         self.test2 = 'texts/test2.txt'
-        self.test2_result = [('Rack', 1), ('Space', 1), ('Managed', 1),
-                             ('Security', 1), ('Engineering', 1), ('San', 1),
-                             ('Antonio', 1), ('Texas', 1), ('USA', 1)]
+        self.test2_result = [('rack', 1), ('space', 1), ('managed', 1),
+                             ('security', 1), ('engineering', 1), ('san', 1),
+                             ('antonio', 1), ('texas', 1), ('usa', 1)]
         self.zen = 'texts/zen.txt'
 
     def test_one(self):
@@ -24,6 +23,6 @@ class TestScheduler(unittest.TestCase):
         zen_word_count = Counter()
         zen_contents = open(self.zen, 'r').readlines()
         for line in zen_contents:
-            line = list(filter(None, tokenizer(line)))
+            line = tokenizer(line)
             zen_word_count.update(line)
         self.assertCountEqual(scheduler([self.zen]), zen_word_count.most_common(10))
